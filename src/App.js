215c58b6449e732c3b917e4132405c82a0d999d2
TDPhuc68 <your_email@example.com>
2025-08-12 10:41:05 +0700

    Update code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import TodoList from "./compoments/TodoList";
+import Textfield from "@atlaskit/textfield";
+import Button from "@atlaskit/button";
 
 function App() {
-    return <TodoList/>;
+    return (
+      <>
+      <h3>Danh sach can lam </h3>
+      <Textfield 
+      name='add-todo'
+      placeholder='Them viec can lam...'
+      elemAfterInput={
+        <Button isDisabled={true} appearance='primary'> Them</Button>
+      }
+      css= {{ padding: "2px 4px 2px"}}
+      ></Textfield>
+      <TodoList/>
+      </>
+    );
 }
 
 export default App;
